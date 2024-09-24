bf5173dfb8c5b42b41798d4120763dd92f435dd4
paulankitapaul <paulankita.ctg@gmail.com>
2024-09-25 00:29:43 +0600

    done calculations and added other cards

--- js/features.js
+++ js/features.js
@@ -7,3 +7,48 @@ document.getElementById('btn-history').addEventListener('click', function () {
     showSection('history-section');
     showActive('btn-history');
 });
+
+
+// Calculate Noakhali Donation
+document.getElementById('btn-donate-noakhali').addEventListener('click', function (event) {
+    event.preventDefault();
+    const inputValue = getInputValue('input-donation-noakhali');
+    const currentBalance = getInnerText('current-balance');
+    const currentDonation = getInnerText('noa-balance');
+
+    const newCurrentBalance = currentBalance - inputValue;
+    const newCurrentDonation = currentDonation + inputValue;
+
+    document.getElementById('current-balance').innerText = newCurrentBalance;
+    document.getElementById('noa-balance').innerText = newCurrentDonation;
+
+});
+
+
+// Calculate Feni Donation
+document.getElementById('btn-donate-feni').addEventListener('click', function (event) {
+    event.preventDefault();
+    const inputValue = getInputValue('input-donation-feni');
+    const currentBalance = getInnerText('current-balance');
+    const currentDonation = getInnerText('feni-balance');
+
+    const newCurrentBalance = currentBalance - inputValue;
+    const newCurrentDonation = currentDonation + inputValue;
+
+    document.getElementById('current-balance').innerText = newCurrentBalance;
+    document.getElementById('feni-balance').innerText = newCurrentDonation;
+});
+
+// Calculate Quota Donation
+document.getElementById('btn-donate-quota').addEventListener('click', function (event) {
+    event.preventDefault();
+    const inputValue = getInputValue('input-donation-quota');
+    const currentBalance = getInnerText('current-balance');
+    const currentDonation = getInnerText('quota-balance');
+
+    const newCurrentBalance = currentBalance - inputValue;
+    const newCurrentDonation = currentDonation + inputValue;
+
+    document.getElementById('current-balance').innerText = newCurrentBalance;
+    document.getElementById('quota-balance').innerText = newCurrentDonation;
+});
--- js/utilities.js
+++ js/utilities.js
@@ -1,7 +1,36 @@
 // change button color onClick
 function showActive(id) {
+
     document.getElementById('btn-donation').classList.remove('bg-[#B4F461]')
+
     document.getElementById('btn-history').classList.remove('bg-[#B4F461]')
+
 
     document.getElementById(id).classList.add('bg-[#B4F461]')
 };
+
+// hiding one of the parts
+function showSection(id) {
+
+    document.getElementById('donation-section').classList.add('hidden');
+
+    document.getElementById('history-section').classList.add('hidden');
+
+
+    document.getElementById(id).classList.remove('hidden');
+};
+
+
+// getting input value
+function getInputValue(id) {
+    const inputValue = document.getElementById(id).value;
+    const donateValue = parseFloat(inputValue);
+    return donateValue;
+}
+
+// getting current amount
+function getInnerText(id) {
+    const balance = document.getElementById(id).innerText;
+    const haveBalance = parseFloat(balance);
+    return haveBalance;
+}
