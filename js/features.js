c9bbb0a9a4ea8ffacc9ebc974f163742a46e0776
paulankitapaul <paulankita.ctg@gmail.com>
2024-09-24 23:52:17 +0600

    added nav and one card

---
+++ js/features.js
@@ -0,0 +1,9 @@
+document.getElementById('btn-donation').addEventListener('click', function () {
+    showSection('donation-section');
+    showActive('btn-donation');
+});
+
+document.getElementById('btn-history').addEventListener('click', function () {
+    showSection('history-section');
+    showActive('btn-history');
+});
